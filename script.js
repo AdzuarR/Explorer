7e350bba1e6114c5f25fbf2d2891c4d750cc2800
adzuarr <adzuar.remy@gmail.com>
2020-01-30 05:31:55 +0100

    script.js, Map class created, index.html table map created, master.css code to collapse perfectly cells of a table added

--- script.js
+++ script.js
@@ -3,10 +3,21 @@ class Case {
   // Object which represent a tile of the game map
   constructor() {
       this.name = "default";
+      this.src = 'https://images-na.ssl-images-amazon.com/images/I/61QX5rSzAsL._AC_SX679_.jpg'
   }
 
   getNom() {
     return this.name;
+  }
+  getSrc(){
+    return this.src;
+  }
+  toImg(width, height){
+    let img = document.createElement('img');
+    img.setAttribute('src', this.src);
+    img.setAttribute('width', width);
+    img.setAttribute('height', height);
+    return img;
   }
 }
 
@@ -44,11 +55,54 @@ class Matrix {
 
 }
 
+class Map {
+  // Used for showing the main fram in html format
+  constructor(matrix){
+    this.matrix = matrix;
+  }
+
+  printMap(){
+    let table = document.getElementById("map");
+    let row = table.insertRow();
+    let cell = row.insertCell();
+    cell.textContent = "prout";
+    let cell2 = row.insertCell();
+    cell2.textContent = "BOB";
+
+
+    while (table.firstChild) {
+      table.removeChild(table.firstChild);
+    }
+
+
+
+    for (let i = 0 ; i < this.matrix.getLines() ; i++){
+
+      let row = table.insertRow();
+
+      for (let j = 0 ; j < this.matrix.getColumns() ; j++){
+
+        let cell = row.insertCell();
+        // For debug`
+        //cell.textContent = "{"+i+"_"+j+"}";
+        cell.appendChild(this.matrix.getValue(i,j).toImg(32,32));
+      }
+
+    }
+
+  }
+
+}
+
 // Test Area
 
 test = new Case();
 matriceTest = new Matrix(10,10,0);
-mTC = new Matrix(5,4,test);
+mTC = new Matrix(80,80,test);
+Mapping = new Map(mTC);
+Mapping.printMap();
+
+console.log(test.getSrc());
 
 console.log(test.name);
 console.log(test.getNom());
