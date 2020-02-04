eb0fac8655c3046fe9ad78e98db1f19f005dcaf2
adzuarr <adzuar.remy@gmail.com>
2020-02-04 20:34:37 +0100

    Map.js, ajout constructeur et ajout fonction printMap()

--- Scripts/Map.js
+++ Scripts/Map.js
@@ -1,8 +1,44 @@
 // Classe à préciser, gérera probablement une partie de la logique ou de
 // L'affiche du joueur
+// Auteur : Adzuar Rémy the Giant snail
+// Copyleft 2020
+
 
 class Map{
-  // PASS
+  /**
+  Classe destiné à gérer le background (Matrice) et les entité rattachée à cette
+  matriceTest
+  */
+
+  constructor(mat){
+    this.matrice = mat;
+  }
+
+  printMap(){
+    /**
+    * Fonction qui rafraichi l'affiche du plateau de jeu dans la table de
+    * index.html
+    */
+    let table = document.getElementById('map');
+
+    // Suppression de l'ancien contenu
+    while (table.firstChild){
+      table.removeChild(table.firstChild);
+    }
+    for (let i = 0; i < this.matrice.getHeight(); i++){
+      let row = table.insertRow();
+
+      for (let j = 0; j < this.matrice.getWidth(); i++){
+        let cell = row.insertCell();
+
+        cell.appendChild(this.matrice.getValue(i,j));
+
+      }
+
+
+    }
+  }
+
 }
 
 /*
@@ -13,3 +49,6 @@ class Map{
 *
 */
 
+Test_map = new Map(Matrice);
+
+Test_map.printMap();
