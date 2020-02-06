a219888bc9c5fab4c80a085efd710cadde274b17
Alexandre Damasceno <alexandre.damasceno@etu.univ-orleans.fr>
2020-02-06 17:35:13 +0100

    Implémentation Base de joueur

--- Scripts/Joueur.js
+++ Scripts/Joueur.js
@@ -2,8 +2,36 @@
 
 
 class Joueur{
-  // PASS
-}
+  constructor(nom,pointsDeVie, attaque, defense,positionX, positionY){
+    this.nom = nom;
+    this.pdv = pointsDeVie;
+    this.attaque = attaque;
+    this.defense = defense;
+    this.x = positionX;
+    this.y = positionY;
+  }
+
+  getNom(){return this.nom;}
+
+  getPointsDeVie(){return this.pointsDeVie;}
+  setPointsDeVie(pointDeVie){ this.pointsDeVie = pointDeVie;}
+
+  getAttaque(){ return this.attaque}
+  setAttaque(att){ this.attaque = att;}
+
+  getDefense(){ return this.defense;}
+  setDefense(def){this.defense = def;}
+
+  getX(){ return this.positionX;}
+  setX(x){this.positionX = x;}
+
+  getY(){return this.positionY;}
+  setY(y){this.positionY = y;}
+
+  couple(){
+    res = this.getX, this.getY;
+    return res;
+  }
 
 /*
 *
@@ -12,3 +40,14 @@ class Joueur{
 * ############################################################################
 *
 */
+
+joueur = new Joueur();
+
+joueur.setPointsDeVie(10);
+console.log("Pointds de vie "+joueur.getPointsDeVie());
+
+joueur.setX(2);
+joueur.setY(1);
+
+joueur.couple();
+
